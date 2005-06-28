be56a5234a21f18b9162f601760914a86df61e95
Aras Pranckevicius <aras@unity3d.com>
2005-06-28 07:24:21 +0000

    Texture bundle always takes miplevels from file for DDS textures (i.e. don't build mips if they're not in the file).

--- dingus/dingus/resource/TextureBundle.cpp
+++ dingus/dingus/resource/TextureBundle.cpp
@@ -6,6 +6,7 @@
 #include "TextureBundle.h"
 #include "../utils/Errors.h"
 #include "../kernel/D3DDevice.h"
+#include "../utils/StringHelper.h"
 
 using namespace dingus;
 
@@ -22,11 +23,18 @@ CTextureBundle::CTextureBundle()
 IDirect3DTexture9* CTextureBundle::loadTexture( const CResourceId& id, const CResourceId& fullName ) const
 {
 	IDirect3DTexture9* texture = NULL;
+
+	// load mips from file in DDS case; construct full mip chain for other
+	// formats
+	DWORD mipLevels = 0;
+	if( CStringHelper::endsWith( fullName.getUniqueName(), ".dds" ) )
+		mipLevels = D3DX_FROM_FILE;
+
 	HRESULT hres = D3DXCreateTextureFromFileEx(
 		&CD3DDevice::getInstance().getDevice(),
 		fullName.getUniqueName().c_str(),
 		D3DX_DEFAULT, D3DX_DEFAULT,
-		0,	// mipLevels
+		mipLevels,
 		0,
 		D3DFMT_UNKNOWN,
 		D3DPOOL_MANAGED,
