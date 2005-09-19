ea0e76ec8868e774a8cec38fc63173392f022f7e
Aras Pranckevicius <aras@unity3d.com>
2005-09-19 16:36:19 +0000

    VG: game setup UI

--- nanobots/src/demo/GameSetupUI.cpp
+++ nanobots/src/demo/GameSetupUI.cpp
@@ -31,14 +31,14 @@ CGameSetupDialog::CGameSetupDialog()
 	mDlg->setFont( 1, "Arial", 22, 50 );
 	mDlg->setFont( 2, "Arial", 14, 50 );
 
-	int yline;
-	const float HC = 18;
-	const float DYLINE = HC+2;
+	//int yline;
+	//const float HC = 18;
+	//const float DYLINE = HC+2;
 
-	CUIStatic* lab;
+	//CUIStatic* lab;
 
-	const int xcol = 30;
-	yline = 10;
+	//const int xcol = 30;
+	//yline = 10;
 
 	/*
 	mDlg->addStatic( 0,
@@ -135,22 +135,9 @@ void CALLBACK CGameSetupDialog::renderCallback( CUIDialog& dlg )
 	yline = dlg.getY() + 10;
 	
 	SFRect rc;
-	rc.set( xcol-10, yline, xcol+400, yline+22 );
-	dlg.imDrawText( "Missions:", 1, DT_LEFT | DT_VCENTER, 0xFFffffff, rc, false );
-
-	yline += 24;
-	rc.set( xcol, yline, xcol+400, yline+DYLINE );
-	dlg.imDrawText( desc.getMissionSummary().c_str(), 0, DT_LEFT|DT_VCENTER, 0xFFffffff, rc, false );
-
-	for( i = 0; i < desc.getMissionCount(); ++i ) {
-		yline += 24;
-		rc.set( xcol, yline, xcol+400, yline+DYLINE );
-		dlg.imDrawText( desc.getMission(i).desc.c_str(), 0, DT_LEFT|DT_VCENTER, 0xFFffffff, rc, false );
-	}
 
 	// ---- players, join buttons etc.
 
-	yline += 24;
 	rc.set( xcol-10, yline, xcol+400, yline+22 );
 	dlg.imDrawText( "Players:", 1, DT_LEFT | DT_VCENTER, 0xFFffffff, rc, false );
 
@@ -160,19 +147,60 @@ void CALLBACK CGameSetupDialog::renderCallback( CUIDialog& dlg )
 	rc.set( xcol, yline, xcol+400, yline+DYLINE );
 	dlg.imDrawText( nplayers==1 ? "Single player game" : "Two player game", 0, DT_LEFT | DT_VCENTER, 0xFFffffff, rc, false );
 
+	const net::SServerState& serverState = mSingleInstance->mServerState;
+
 	static const char* PLAYER_TEX_NAMES[G_MAX_PLAYERS] = {
 		NULL,
 		RID_TEX_PLAYER1,
 		RID_TEX_PLAYER2,
 	};
 	for( i = 1; i < desc.getPlayerCount(); ++i ) {
-		yline += 24;
-		rc.set( xcol, yline, xcol+400, yline+DYLINE );
-		dlg.imDrawText( desc.getPlayer(i).name.c_str(), 0, DT_LEFT|DT_VCENTER, 0xFFffffff, rc, false );
+		char buf[100];
 
-		rc.set( xcol+100, yline, xcol+100+DYLINE, yline+DYLINE );
+		yline += DYLINE;
+
+		// label
+		sprintf( buf, "Player %i:", i );
+		rc.set( xcol, yline, xcol+60, yline+DYLINE );
+		dlg.imDrawText( buf, 0, DT_LEFT|DT_VCENTER, 0xFFc0c0c0, rc, false );
+
+		// flag
+		rc.set( xcol+40+1, yline+1, xcol+40+DYLINE-2, yline+DYLINE-2 );
 		RECT uvrc = { 0, 0, CGameDesc::FLAG_SIZE, CGameDesc::FLAG_SIZE };
 		dlg.imDrawSprite( 0xFFffffff, uvrc, RGET_S_TEX(PLAYER_TEX_NAMES[i]), rc );
+
+		// name
+		rc.set( xcol+40+DYLINE+5, yline, xcol+400, yline+DYLINE );
+		dlg.imDrawText( desc.getPlayer(i).name.c_str(), 0, DT_LEFT|DT_VCENTER, 0xFFffffff, rc, false );
+
+		// state
+		const char* state = "[replay]";
+		if( serverState.playerRealtime[i] ) {
+			state = "Realtime, join!";
+			if( serverState.playerControlled[i] ) {
+				state = "Realtime, controlled";
+			}
+		}
+		rc.set( xcol+200, yline, xcol+400, yline+DYLINE );
+		dlg.imDrawText( state, 0, DT_LEFT|DT_VCENTER, 0xFFffffff, rc, false );
+	}
+
+	// ---- missions
+
+	yline += 24;
+	rc.set( xcol-10, yline, xcol+400, yline+22 );
+	dlg.imDrawText( "Missions:", 1, DT_LEFT | DT_VCENTER, 0xFFffffff, rc, false );
+
+	if( !desc.getMissionSummary().empty() ) {
+		yline += 24;
+		rc.set( xcol, yline, xcol+400, yline+DYLINE );
+		dlg.imDrawText( desc.getMissionSummary().c_str(), 0, DT_LEFT|DT_VCENTER, 0xFFffffff, rc, false );
+	}
+
+	for( i = 0; i < desc.getMissionCount(); ++i ) {
+		yline += 24;
+		rc.set( xcol, yline, xcol+400, yline+DYLINE );
+		dlg.imDrawText( desc.getMission(i).desc.c_str(), 0, DT_LEFT|DT_VCENTER, 0xFFffffff, rc, false );
 	}
 }
 
